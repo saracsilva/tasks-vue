fc270063e4d4fb2cf6916275a225111b5de04281
saracsilva <saracarolinasilva@gmail.com>
2023-02-17 11:41:17 +0100

    Created Header and Button component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>Task Traker</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "HeaderOne",
+  components: {
+    Button,
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
